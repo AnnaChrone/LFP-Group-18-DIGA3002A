b0eedf090bf59634c424cc4a4460a2608b761c1e
AnnaChrone <jnchronis1@gmail.com>
2026-09-07 18:50:50 +0200

    GameScene | Basic Character Movement

---
+++ Assets/Scripts/Player Controls/PlayerControls.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private int facingDirection = 1;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+
+        if (moveInput.x > 0 && facingDirection == -1 ||
+            moveInput.x < 0 && facingDirection == 1)
+        {
+            //Flip();
+        }
+    }
+
+    /*void Flip() WHEN WE HAVE A SPRITE
+    {
+        facingDirection *= -1;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }*/
+
+    public void OnMovement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log($"OnMovement fired: {moveInput}");
+    }
+}
